a89e0d494dc8f8fba8a23be680db840d2f702dc8
Anirban Basak <jeetbasak2002@gmail.com>
2024-12-05 09:34:43 +0530

    initial commit

---
+++ src/main/java/dev/anirban/stridesync/StridesyncApplication.java
@@ -0,0 +1,13 @@
+package dev.anirban.stridesync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StridesyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StridesyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/anirban/stridesync/StridesyncApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.anirban.stridesync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StridesyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
