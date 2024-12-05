00be9a91368ecc446d133e3bc893968724b15ae1
Anirban Basak <jeetbasak2002@gmail.com>
2024-12-05 12:19:29 +0530

    refactor - minor changes.

--- src/main/java/dev/anirban/stridesync/constants/UrlConstants.java
+++ src/main/java/dev/anirban/stridesync/constants/UrlConstants.java
@@ -2,7 +2,7 @@ package dev.anirban.stridesync.constants;
 
 public class UrlConstants {
 
-    // Authentication Endpoints (Librarian)
+    // Authentication Endpoints
     public static final String REGISTER_USER = "/register";
     public static final String LOGIN_USER = "/login";
 }
